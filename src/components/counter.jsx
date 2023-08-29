458b82a6b34d84d12185bde5d2e69cf99f7aeac6
Vatsal Pathak <vatsal.pathak@dskservices.com>
2023-08-29 15:54:01 -0700

    Pushing initial app

---
+++ src/components/counter.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 1,
+  };
+
+  // constructor() {
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+  doHandleIncrement = () => {
+    this.handleIncrement({ id: 1 });
+  };
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+  //   return (
+  //     <ol>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ol>
+  //   );
+  // }
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeMethod()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+  getBadgeMethod() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Counter from "./components/counter";
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>
 );
 
