5f2518da22765f19c54b63d94e71c517ecfdd49b
Vatsal Pathak <vatsal.pathak@dskservices.com>
2023-08-29 16:39:38 -0700

    Until props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    value: this.props.value,
   };
 
   // constructor() {
@@ -11,7 +11,7 @@ class Counter extends Component {
   // }
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
   doHandleIncrement = () => {
     this.handleIncrement({ id: 1 });
@@ -29,7 +29,7 @@ class Counter extends Component {
   // }
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeMethod()}>{this.formatCount()}</span>
         <button
           onClick={() => this.handleIncrement({ id: 1 })}
@@ -37,17 +37,17 @@ class Counter extends Component {
         >
           Increment
         </button>
-      </React.Fragment>
+      </div>
     );
   }
   getBadgeMethod() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Counter from "./components/counter";
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+import Counters from "./components/counters";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>
-);
+root.render(<Counters />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
